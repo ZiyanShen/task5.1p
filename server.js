bd3d5bc3131b8d07f4da2fead7d0621f9a740c74
zh601095001 <601095001@qq.com>
2022-08-17 17:32:38 +0800

    task4.1P

--- server.js
+++ server.js
@@ -4,13 +4,11 @@ const app = express();
 const path = require('path');
 const {logger} = require("./middleware/logEvents");
 const errorHandler = require("./middleware/errorHandler");
-// const expressStatic = require("express-static")
+
 
 const PORT = process.env.PORT || 3500;
-// app.use('/static', express.static(path.join(__dirname, '/public/static')));
-// app.use('/images', express.static(path.join(__dirname, '/public/images')));
+
 app.use('/', express.static(path.join(__dirname, '/public')));
-// app.use(expressStatic("./public"))
 
 // logger
 app.use(logger)
